ed3a378ab2ba371c5a8ce6fac56acaf699aa3947
Brayden Bos <32762@ma-web.nl>
2022-03-30 21:47:09 +0200

    2e sprint review

--- Arcade Mechanics/Assets/scrips/PlayerMovements.cs
+++ Arcade Mechanics/Assets/scrips/PlayerMovements.cs
@@ -10,15 +10,13 @@ public class PlayerMovements : MonoBehaviour
     public CharacterController controller;
     private Vector3 moveDirection;
     public float gravityScale;
+    public Animator animator;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
@@ -27,13 +25,28 @@ public class PlayerMovements : MonoBehaviour
         moveDirection = moveDirection.normalized * moveSpeed;
         moveDirection.y = yStore;
 
+        if(moveDirection.z > 0||moveDirection.x > 0)
+        {
+            animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
         if (controller.isGrounded)
         {
+            animator.SetBool("InAir", false);
+            animator.SetBool("Jump", false);
             moveDirection.y = -1f;
             if (Input.GetButtonDown("Jump"))
             {
+                animator.SetBool("Jump", true);
                 moveDirection.y = jumpForce;
             }
+        }
+        else
+        {
+            animator.SetBool("InAir", true);
         }
         moveDirection.y += Physics.gravity.y * gravityScale * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
